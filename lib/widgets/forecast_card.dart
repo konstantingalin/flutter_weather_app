6b61ab8f3fef745d89b2870a6ce7668a114aaf54
Konstantin <the.it.promo@gmail.com>
2023-03-11 21:47:12 +0600

    add forecast card

---
+++ lib/widgets/forecast_card.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class ForecastCard extends StatelessWidget {
+  ForecastCard({super.key, required this.hour, required this.description, required this.descriptionIMG, required this.averageTemp});
+
+  String hour;
+  String description;
+  String descriptionIMG;
+  double averageTemp;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 120,
+      height: 170,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: const Color.fromARGB(18, 255, 255, 2555),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            hour, style: const TextStyle(
+              color: Colors.white, fontWeight: FontWeight.w600,
+            ),
+          ),
+          const SizedBox( height: 10,),
+          Container(
+            width: 70,
+            height: 70,
+            color: Colors.black,
+            child: (Image.network(descriptionIMG, fit: BoxFit.cover,)),
+          ),
+          const SizedBox(height: 10,),
+          Text(
+            "$averageTemp°",
+            style: const TextStyle(fontWeight: FontWeight.w600, color: Colors.white),
+          ),
+          const SizedBox(height: 6,),
+          Text(
+            description,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              color: Color.fromARGB(139, 255, 255, 255),
+              fontWeight: FontWeight.w600,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
